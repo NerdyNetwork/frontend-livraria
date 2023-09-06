65e5489478e0cf4502f916a3a29eaf5394435b0c
JoaoPedroli <joaopedrocz12345@gmail.com>
2023-09-06 19:39:04 -0300

    update modal

--- src/components/Modal/ModalItem/index.tsx
+++ src/components/Modal/ModalItem/index.tsx
@@ -1,10 +1,14 @@
 import styles from "./styles.module.scss";
+import image from "../../../assets/bookimage.jpeg";
 
 export const ModalItem = () => {
   return (
     <div className={styles["container"]}>
-      <h3>Mindset</h3>
-      <span>R$ 35,90</span>
+      <img src={image} />
+      <div className={styles["subcontainer"]}>
+        <h3>Mindset</h3>
+        <span>R$ 35,90</span>
+      </div>
     </div>
   );
 };
